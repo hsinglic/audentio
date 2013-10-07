819e2078352628f0540e65417423d32044aa087c
hsinglic <hsingilic@gmail.com>
2013-10-07 14:15:39 -0600

    initial

---
+++ app/Config/database.php
@@ -0,0 +1,26 @@
+<?php
+class DATABASE_CONFIG {
+
+	public $default = array(
+		'datasource' => 'Database/Mysql',
+		'persistent' => false,
+		'host' => '69.194.230.51',
+		'login' => 'guatemal',
+		'password' => 'mnQd,8hraVp8',
+		'database' => 'guatemal_db',
+		'prefix' => '',
+		//'encoding' => 'utf8',
+	);
+
+	public $test = array(
+		'datasource' => 'Database/Mysql',
+		'persistent' => false,
+		'host' => '69.194.230.51',
+		'login' => 'guatemal',
+		'password' => 'mnQd,8hraVp8',
+		'database' => 'guatemal_test_db',
+		'prefix' => '',
+		//'encoding' => 'utf8',
+	);
+}
+?>
