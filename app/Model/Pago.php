b030f44dff5f32ac0f09832656f7cb8414577315
wichovw <wichovalde@gmail.com>
2013-10-16 05:52:41 -0600

    dummy de Pago

---
+++ app/Model/Pago.php
@@ -0,0 +1,11 @@
+<?php
+App::uses('AppModel', 'Model');
+
+class Pago extends AppModel {
+    
+	public function guardarAbonoProyecto($projeectid, $valor){
+		// Ingresa un abono de un proyecto a la base  de datos
+		return 0;
+	}
+}
+?>
